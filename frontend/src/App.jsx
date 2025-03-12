d6c07056ba9b90a048664f99e51141a4169802a3
Will Anderson <WillAnderson1339@gmail.com>
2025-03-12 15:54:38 -0700

    Initial Basic Project Create

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import Header from './components/Header'
+
+import './css/App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>
+        <p>Hello World!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import '../css/Header.css'
+
+// import memeGeneratorLogo from '/Chef 1.png'
+
+function Header() {
+    return (
+        <header>
+            {/* <img className="nav-image" src={memeGeneratorLogo} alt="Meme Generator Logo"/> */}
+            <h4>Header Component here</h4>
+        </header>
+    )
+}
+
+export default Header
